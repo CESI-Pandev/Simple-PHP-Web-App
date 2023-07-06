618fca2a37f25780a7c0eb4a4ed42035056cea26
Marie <marie.goepp@viacesi.fr>
2023-07-06 15:31:30 +0200

    3-ShowAppVersion
    - version.php created
    - included into footer.php

--- app/www/footer.php
+++ app/www/footer.php
@@ -1,3 +1,5 @@
+
+
 <footer class="page-footer white">
       <h4 style="color: white; padding: 3%" class="center light-blue  contact_text">Contact Us</h4>
 
@@ -47,7 +49,9 @@
     </div>
     <div class="footer-copyright light-blue">
       <div class="container">
-      Made by <a class="orange-text text-lighten-3" href="http://#">JL</a>
+      <?$_ENV['VERSION']?>
+      <?php include 'version.php'; ?> Made by <a class="orange-text text-lighten-3" href="http://#">JL</a>
+      
 
       </div>
     </div>
